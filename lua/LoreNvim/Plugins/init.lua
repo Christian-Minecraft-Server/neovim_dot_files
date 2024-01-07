ce909a84fa92ecaa104050d7c13844d701715bd5
Christian Gonzalez <christian.justiec.gonzalez@gmail.com>
2024-01-07 07:42:08 -0500

    setup vimbegood and fixed some formatting errors

--- lua/LoreNvim/Plugins/init.lua
+++ lua/LoreNvim/Plugins/init.lua
@@ -1,29 +1,30 @@
 return {
-	{ -- LSP
-		'williamboman/mason-lspconfig.nvim',
-		dependencies = {
-			'williamboman/mason.nvim',
-			'neovim/nvim-lspconfig'
-		}
-	},
-	{ -- Autocompletion & Snippets
-		'hrsh7th/nvim-cmp',
-		dependencies = {
-			'hrsh7th/cmp-nvim-lsp',
-			'hrsh7th/cmp-buffer',
-			'hrsh7th/cmp-path',
-			'hrsh7th/cmp-cmdline',
-			-- lua snip
-			{'L3MON4D3/LuaSnip', build = 'make install_jsregexp'},
-			'saadparwaiz1/cmp_luasnip'
-		}
-	},
-	{ -- Telescope
-		'nvim-telescope/telescope.nvim',
-		dependencies = {
-			'nvim-lua/plenary.nvim'
-		}
-	},
-    "EdenEast/nightfox.nvim", -- Nightfox colorschemes
-    {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}, -- Treesitter
+    { -- LSP
+        'williamboman/mason-lspconfig.nvim',
+        dependencies = {
+            'williamboman/mason.nvim',
+            'neovim/nvim-lspconfig'
+        }
+    },
+    { -- Autocompletion & Snippets
+        'hrsh7th/nvim-cmp',
+        dependencies = {
+            'hrsh7th/cmp-nvim-lsp',
+            'hrsh7th/cmp-buffer',
+            'hrsh7th/cmp-path',
+            'hrsh7th/cmp-cmdline',
+            -- lua snip
+            {'L3MON4D3/LuaSnip', build = 'make install_jsregexp'},
+            'saadparwaiz1/cmp_luasnip'
+        }
+    },
+    { -- Telescope
+        'nvim-telescope/telescope.nvim',
+        dependencies = {
+            'nvim-lua/plenary.nvim'
+        }
+    },
+    {'nvim-treesitter/nvim-treesitter', build = ':TSUpdate'}, -- Treesitter
+    'EdenEast/nightfox.nvim', -- Nightfox colorschemes
+    'ThePrimeagen/vim-be-good'
 }
