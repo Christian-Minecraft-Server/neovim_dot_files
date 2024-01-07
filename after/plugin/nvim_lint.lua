fb7fa9c37d481d0686c88a063278ce4918f83a6d
Christian Gonzalez <christian.justiec.gonzalez@gmail.com>
2024-01-07 09:29:44 -0500

    setup linter and formatter for go and lua

---
+++ after/plugin/nvim_lint.lua
@@ -0,0 +1,21 @@
+local lint = require("lint")
+local golangci = lint.linters.golangcilint
+golangci.args = { "--enable-all" }
+lint.linters_by_ft = {
+	go = { "golangcilint" },
+	lua = { "luacheck" },
+}
+
+-- Lints on enter buffer, write to buffer, leave insert
+local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+	--pattern = '*.go', -- Limit running linter to files with names that match the pattern
+	group = lint_augroup,
+	callback = function()
+		lint.try_lint()
+	end,
+})
+
+vim.keymap.set("n", "<leader>l", function()
+	lint.try_lint()
+end, { desc = "Trigger linting on current file via keypress" })
